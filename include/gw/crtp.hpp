ed7988bb9e0e77ec991ed04e793b04aae9b218dc
Martin Stump <11492152+globberwops@users.noreply.github.com>
2023-12-19 13:37:43 +0100

    Refactor CRTP constraints based on std::ranges::view_interface
    Signed-off-by: Martin Stump <11492152+globberwops@users.noreply.github.com>

--- include/gw/crtp.hpp
+++ include/gw/crtp.hpp
@@ -3,18 +3,25 @@
 
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 
 namespace gw {
 
 template <template <typename> class T, typename Derived>
-concept crtp_concept = requires { std::is_base_of_v<T<Derived>, Derived>; };
-
-template <template <typename> class T, typename Derived>
-  requires crtp_concept<T, Derived>
+  requires std::is_class_v<Derived> && std::same_as<Derived, std::remove_cv_t<Derived>>
 struct crtp {
-  auto self() noexcept -> Derived& { return static_cast<Derived&>(*this); }
-  auto self() const noexcept -> Derived const& { return static_cast<Derived const&>(*this); }
+  constexpr auto self() noexcept -> Derived& {
+    static_assert(std::derived_from<Derived, T<Derived>>);
+    return static_cast<Derived&>(*this);
+  }
+
+  constexpr auto self() const noexcept -> Derived const& {
+    static_assert(std::derived_from<Derived, T<Derived>>);
+    return static_cast<const Derived&>(*this);
+  }
+
+  constexpr auto operator<=>(const crtp&) const noexcept = default;
 
  private:
   crtp() = default;
